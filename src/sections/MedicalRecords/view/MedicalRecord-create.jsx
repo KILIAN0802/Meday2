8bb434d3778179440f96ee4280c08e411978fbe8
nguyendat <nguyendat08022004@gmail.com>
2025-08-12 11:16:43 +0700

    ab

--- src/sections/MedicalRecords/view/MedicalRecord-create.jsx
+++ src/sections/MedicalRecords/view/MedicalRecord-create.jsx
@@ -2,25 +2,28 @@
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import { Add as AddIcon } from '@mui/icons-material';
+import { Add as AddIcon, WidthFull } from '@mui/icons-material';
 import { RouterLink } from 'src/routes/components';
 import { paths } from 'src/routes/paths';
 // ----------------------------------------------------------------------
 
 export function MedicalRecordsCreate() {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, maxWidth: 420 }}>
-      <Button component={RouterLink} href = {paths.dashboard.MedicalRecords.chronic } variant="contained" startIcon={<AddIcon />} fullWidth>
-        Tạo bệnh án mạn tính lần 1
-      </Button>
+    <Box sx={{ display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center', flexDirection: 'column'}}>
+      <h1 >Tạo bệnh án 📜</h1>
+        <Box sx={{ display: 'flex', gap: 2, maxWidth: 420, border:'1px solid black', padding: '10px', borderRadius: '15px'}}>
+        <Button component={RouterLink} href = {paths.dashboard.MedicalRecords.chronic } variant="contained" startIcon={<AddIcon />}  fullWidth sx={{ transition: 'transform 0.2s ease-in-out', '&:hover': {transform: 'scale(1.05)',  }, }}>
+          Tạo bệnh án mạn tính lần 1
+        </Button>
 
-      <Button omponent={RouterLink} href = {paths.dashboard.MedicalRecords.rechronic } variant="contained" color="secondary" startIcon={<AddIcon />} fullWidth>
-        Tạo bệnh án mạn tính tái khám
-      </Button>
+        <Button omponent={RouterLink} href = {paths.dashboard.MedicalRecords.rechronic } variant="contained"  startIcon={<AddIcon />}  fullWidth sx={{ transition: 'transform 0.2s ease-in-out', '&:hover': {transform: 'scale(1.05)',  }, }}>
+          Tạo bệnh án mạn tính tái khám
+        </Button>
 
-      <Button omponent={RouterLink} href = {paths.dashboard.MedicalRecords.acute } variant="contained" color="warning" startIcon={<AddIcon />} fullWidth>
-        Tạo bệnh án cấp tính
-      </Button>
+        <Button omponent={RouterLink} href = {paths.dashboard.MedicalRecords.acute } variant="contained"  startIcon={<AddIcon />}  fullWidth sx={{ transition: 'transform 0.2s ease-in-out', '&:hover': {transform: 'scale(1.05)',  }, }}>
+          Tạo bệnh án cấp tính
+        </Button>
+      </Box>
     </Box>
   );
 }
