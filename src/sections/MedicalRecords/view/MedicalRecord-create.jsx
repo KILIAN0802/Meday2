3a6bc0044d048436a6fab982d7e42ae5ebc79308
nguyendat <nguyendat08022004@gmail.com>
2025-08-12 14:51:19 +0700

    abc

--- src/sections/MedicalRecords/view/MedicalRecord-create.jsx
+++ src/sections/MedicalRecords/view/MedicalRecord-create.jsx
@@ -9,18 +9,18 @@ import { paths } from 'src/routes/paths';
 
 export function MedicalRecordsCreate() {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center', flexDirection: 'column'}}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
       <h1 >Tạo bệnh án 📜</h1>
-        <Box sx={{ display: 'flex', gap: 2, maxWidth: 420, border:'1px solid black', padding: '10px', borderRadius: '15px'}}>
+        <Box sx={{ display: 'flex', gap: 2, maxWidth: 420, border:'2px solid black', padding: '10px', borderRadius: '20px'}}>
         <Button component={RouterLink} href = {paths.dashboard.MedicalRecords.chronic } variant="contained" startIcon={<AddIcon />}  fullWidth sx={{ transition: 'transform 0.2s ease-in-out', '&:hover': {transform: 'scale(1.05)',  }, }}>
           Tạo bệnh án mạn tính lần 1
         </Button>
 
-        <Button omponent={RouterLink} href = {paths.dashboard.MedicalRecords.rechronic } variant="contained"  startIcon={<AddIcon />}  fullWidth sx={{ transition: 'transform 0.2s ease-in-out', '&:hover': {transform: 'scale(1.05)',  }, }}>
+        <Button omponent={RouterLink} href = {paths.dashboard.MedicalRecords.rechronic } variant="contained"  startIcon={<AddIcon />}  color='secondary' fullWidth sx={{ transition: 'transform 0.2s ease-in-out', '&:hover': {transform: 'scale(1.05)',  }, }}>
           Tạo bệnh án mạn tính tái khám
         </Button>
 
-        <Button omponent={RouterLink} href = {paths.dashboard.MedicalRecords.acute } variant="contained"  startIcon={<AddIcon />}  fullWidth sx={{ transition: 'transform 0.2s ease-in-out', '&:hover': {transform: 'scale(1.05)',  }, }}>
+        <Button omponent={RouterLink} href = {paths.dashboard.MedicalRecords.acute } variant="contained"  startIcon={<AddIcon />}  color='warning' fullWidth sx={{ transition: 'transform 0.2s ease-in-out', '&:hover': {transform: 'scale(1.05)',  }, }}>
           Tạo bệnh án cấp tính
         </Button>
       </Box>
